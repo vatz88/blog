d58e68cc2ecc4da87f0f032ef49a7dcb980aeb04
Vatsal Joshi <vatz88@gmail.com>
2018-05-03 00:21:21 +0530

    :sparkles: styling improved

--- src/js/script.js
+++ src/js/script.js
@@ -1,44 +1,44 @@
-var $pre = document.querySelectorAll('pre');
-for (var i = 0; i < $pre.length; i++) {
-    var $span = document.createElement('span');
-    $span.classList.add('copyCode');
-    $span.addEventListener('click', function (e) {
-        try {
-            document.designMode = 'on';
-            window.getSelection().removeAllRanges();
-            const range = document.createRange();
-            range.selectNode(e.target.nextSibling);
-            window.getSelection().addRange(range);
-            const copyStatus = document.execCommand('copy');
-            console.log("Copied ", copyStatus);
-            if (copyStatus) {
-                makeTaost("Copied!");
-            } else {
-                makeTaost("Couldn't copy. Try again.");
-            }
-            window.getSelection().removeAllRanges();
-            document.designMode = 'off';
-        } catch (err) {
-            makeTaost("Something went wrong! Couldn't copy :(");
-            console.log('Err in copy', err);
-        }
-    });
-    $pre.item(i).insertBefore($span, $pre.item(i).firstChild);
-}
-
 // Toast
 var snackBar = document.createElement('div');
-snackBar.id = "snackbar";
-document.getElementsByTagName("body")[0].appendChild(snackBar);
+snackBar.id = 'snackbar';
+document.getElementsByTagName('body')[0].appendChild(snackBar);
 
 function makeTaost(str) {
-    snackBar.innerText = str;
-    snackBar.className = "show";
-    setTimeout(function () {
-        snackBar.className = snackBar.className.replace("show", "");
-    }, 3000);
+  snackBar.innerText = str;
+  snackBar.className = 'show';
+  setTimeout(function() {
+    snackBar.className = snackBar.className.replace('show', '');
+  }, 3000);
+}
+
+// Copy code
+var $pre = document.querySelectorAll('pre');
+for (var i = 0; i < $pre.length; i++) {
+  var $span = document.createElement('span');
+  $span.classList.add('copyCode');
+  $span.addEventListener('click', function(e) {
+    try {
+      document.designMode = 'on';
+      window.getSelection().removeAllRanges();
+      const range = document.createRange();
+      range.selectNode(e.target.nextSibling);
+      window.getSelection().addRange(range);
+      const copyStatus = document.execCommand('copy');
+      if (copyStatus) {
+        makeTaost('Copied!');
+      } else {
+        makeTaost("Couldn't copy. Try again.");
+      }
+      window.getSelection().removeAllRanges();
+      document.designMode = 'off';
+    } catch (err) {
+      makeTaost("Something went wrong! Couldn't copy :(");
+      console.log('Err in copy ', err);
+    }
+  });
+  $pre.item(i).insertBefore($span, $pre.item(i).firstChild);
 }
 
 // console ;)
 var consolestyles = ['font-size: 46px', 'color: #757575'].join(';');
-console.log('%cVaTz88', consolestyles);
+console.log('%cvatz88', consolestyles);
