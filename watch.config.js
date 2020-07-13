5307915a40637c28ab4bba33477bfbd9063f938d
Vatsal Joshi <vatz88@gmail.com>
2020-07-13 17:24:19 +0530

    don't watch changes in node_modules, yarn.lock

--- watch.config.js
+++ watch.config.js
@@ -1,5 +1,8 @@
 module.exports = function filter(params) {
 	return !(
-		String(params).endsWith('.html') || String(params).endsWith('.json')
+		String(params).endsWith('.html') ||
+		String(params).endsWith('.json') ||
+		String(params).includes('node_modules') ||
+		String(params).includes('yarn.lock')
 	);
 };
